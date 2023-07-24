fae870077ca1b189d6d27e9736da89a7cb603365
Monjur-Alam <munjur533@gmail.com>
2023-07-24 19:31:25 +0600

    Initial Commit

---
+++ SwiftUI_AtoZ/SwiftUI_AtoZApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AtoZApp.swift
+//  SwiftUI_AtoZ
+//
+//  Created by MacBook Pro on 24/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AtoZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
